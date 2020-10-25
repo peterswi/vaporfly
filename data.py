fc0498f6b2a1e125fccc3acc6d637b2db91f15e9
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-10-25 15:31:39 -0400

    not quite there with requests, but a start

--- data.py
+++ data.py
@@ -1,11 +1,53 @@
-import pandas as pd
+import requests
+from pandas import json_normalize
+import json
+import csv
 
+"""
 # Initialize the dataframe
-col_names = ['id','type']
-activities = pd.DataFrame(columns=col_names)
+    col_names = ['id','type']
+    activities = pd.DataFrame(columns=col_names)
+    
+    auth code: a1f8da7896c3909ce7ee0dd3933a425320f21598
 
-access_token = "access_token=20a880dd4cb02eea54565bf2e84618f9a034ec3e" # replace with your access token here
+
+    access_token = "access_token=a9751d7b269bcd1e4239bf64f35005f08537eb15" # replace with your access token here
+    url = "https://www.strava.com/api/v3/athlete/activities"
+
+    page = 1
+
+    while True:
+        
+        # get page of activities from Strava
+        r = requests.get(url + '?' + access_token + '&per_page=50' + '&page=' + str(page))
+        r = r.json()
+        print('data',r)
+        # if no results then exit loop
+        if (not r):
+            break
+        
+        # otherwise add new data to dataframe
+        for x in range(len(r)):
+            activities.loc[x + (page-1)*50,'id'] = r[x]['id']
+            activities.loc[x + (page-1)*50,'type'] = r[x]['type']
+
+        # increment page
+        page += 1
+
+    print('done')
+
+"""
+
+
+# Get the tokens from file to connect to Strava
+with open('strava_tokens.json') as json_file:
+    strava_tokens = json.load(json_file)
+# Loop through all activities
 url = "https://www.strava.com/api/v3/activities"
-
-print('done')
-
+access_token = strava_tokens['access_token']
+# Get first page of activities from Strava with all fields
+r = requests.get(url + '?access_token=' + access_token)
+r = r.json()
+    
+df = json_normalize(r)
+df.to_csv('strava_activities_all_fields.csv')
---
+++ get_tokens.py
@@ -0,0 +1,23 @@
+import requests
+import json
+# Make Strava auth API call with your 
+# client_code, client_secret and code
+response = requests.post(
+                    url = 'https://www.strava.com/oauth/token',
+                    data = {
+                            'client_id': 55292,
+                            'client_secret': '3da842c724a8fcf8a2972c5b96996e19d4d9d189',
+                            'code': 'a1f8da7896c3909ce7ee0dd3933a425320f21598',
+                            'grant_type': 'authorization_code'
+                            }
+                )
+#Save json response as a variable
+strava_tokens = response.json()
+# Save tokens to file
+with open('strava_tokens.json', 'w') as outfile:
+    json.dump(strava_tokens, outfile)
+# Open JSON file and print the file contents 
+# to check it's worked properly
+with open('strava_tokens.json') as check:
+  data = json.load(check)
+print(data)
